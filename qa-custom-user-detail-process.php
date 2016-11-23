c2ce36abf655c93742f0a3f378e972b435de2758
yaimapp <gmonday730-mbp@yahoo.co.jp>
2016-11-23 10:26:16 +0900

    プロセスモジュールファイル追加

---
+++ qa-custom-user-detail-process.php
@@ -0,0 +1,65 @@
+<?php
+
+if (!defined('QA_VERSION')) { // don't allow this page to be requested directly from browser
+    header('Location: ../../');
+    exit;
+}
+
+class qa_custom_user_detail_process
+{
+    public function init_page()
+    {
+        $page = qa_request_part(0);
+        $action = qa_request_part(2);
+        if ($page !== 'user' || !empty($action)) {
+            return;
+        }
+
+        require_once QA_INCLUDE_DIR.'app/cookies.php';
+        require_once QA_INCLUDE_DIR.'app/format.php';
+        require_once QA_INCLUDE_DIR.'app/users.php';
+        require_once QA_INCLUDE_DIR.'app/options.php';
+        require_once QA_INCLUDE_DIR.'db/selects.php';
+
+        global $qa_usage;
+
+        qa_report_process_stage('init_page');
+        qa_db_connect('qa_page_db_fail_handler');
+
+        qa_page_queue_pending();
+        qa_load_state();
+        qa_check_login_modules();
+
+        if (QA_DEBUG_PERFORMANCE)
+            $qa_usage->mark('setup');
+
+        qa_check_page_clicks();
+        //    Determine the identify of the user
+
+        $handle = qa_request_part(1);
+
+        if (!strlen($handle)) {
+            $handle = qa_get_logged_in_handle();
+            qa_redirect(!empty($handle) ? 'user/'.$handle : 'users');
+        }
+
+        //    Get the HTML to display for the handle, and if we're using external users, determine the userid
+
+        if (QA_FINAL_EXTERNAL_USERS) {
+            $userid = qa_handle_to_userid($handle);
+            if (!isset($userid))
+                return include QA_INCLUDE_DIR.'qa-page-not-found.php';
+
+            $usershtml = qa_get_users_html(array($userid), false, qa_path_to_root(), true);
+            $userhtml = @$usershtml[$userid];
+
+        } else {
+            $userhtml = qa_html($handle);
+        }
+
+        qa_set_template('user-profile');
+        $qa_content = include CUSTOM_USER_DETAIL_DIR.'/user-profile.php';
+        
+        return $qa_content;
+    }
+}
