8c42b0651a17e7a8393e0854b51093cf7e40d38d
yaimapp <gmonday730-mbp@yahoo.co.jp>
2016-11-23 10:05:22 +0900

    Pluginファイル追加

---
+++ qa-plugin.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+	Plugin Name: Custom User Detail Plugin
+	Plugin URI:
+	Plugin Description: Provide a new user profile page
+	Plugin Version: 1.0
+	Plugin Date: 2016-11-23
+	Plugin Author: 38qa.net
+	Plugin Author URI: http://38qa.net/
+	Plugin License: GPLv2
+	Plugin Minimum Question2Answer Version: 1.7
+	Plugin Update Check URI:
+*/
+
+if (!defined('QA_VERSION')) { // don't allow this page to be requested directly from browser
+	header('Location: ../../');
+	exit;
+}
