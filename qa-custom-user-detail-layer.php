79bc23fc4deb7f44ef51aab97c28aad1d2065bdd
yaimapp <gmonday730-mbp@yahoo.co.jp>
2016-11-24 15:25:58 +0900

    メインコンテンツの表示はプラグイン内のクラスで行う

--- qa-custom-user-detail-layer.php
+++ qa-custom-user-detail-layer.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once CUSTOM_USER_DETAIL_DIR.'/cud-theme-main.php';
+
 class qa_html_theme_layer extends qa_html_theme_base
 {
     public function body_content()
@@ -10,4 +12,14 @@ class qa_html_theme_layer extends qa_html_theme_base
         }
         qa_html_theme_base::body_content();
     }
+    
+    public function main()
+    {
+        if ($this->template === 'user') {
+            cud_theme_main::main($this);
+        } else {
+            qa_html_theme_base::main();
+        }
+    }
+    
 }
