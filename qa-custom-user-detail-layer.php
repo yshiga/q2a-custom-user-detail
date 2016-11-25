9deee5efce45714a518583a18ae547ae5384e55d
yaimapp <gmonday730-mbp@yahoo.co.jp>
2016-11-25 09:44:58 +0900

    テーマがq2a-material-liteのときだけの条件追加

--- qa-custom-user-detail-layer.php
+++ qa-custom-user-detail-layer.php
@@ -1,12 +1,12 @@
 <?php
 
-require_once CUSTOM_USER_DETAIL_DIR.'/cud-theme-main.php';
+require_once CUD_DIR.'/cud-theme-main.php';
 
 class qa_html_theme_layer extends qa_html_theme_base
 {
     public function body_content()
     {
-        if ($this->template === 'user') {
+        if (qa_opt('site_theme') === CUD_TARGET_THEME_NAME && $this->template === 'user') {
             // もともと入っているサブナビゲーションは使用しない
             unset($this->content['navigation']['sub']);
         }
@@ -15,7 +15,7 @@ class qa_html_theme_layer extends qa_html_theme_base
     
     public function main()
     {
-        if ($this->template === 'user') {
+        if (qa_opt('site_theme') === CUD_TARGET_THEME_NAME && $this->template === 'user') {
             cud_theme_main::main($this);
         } else {
             qa_html_theme_base::main();
