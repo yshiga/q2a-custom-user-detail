2faf2526fb588eeaf8130ed646b28fd93b311433
yaimapp <gmonday730-mbp@yahoo.co.jp>
2016-11-29 09:47:12 +0900

    無限スクロール用のスクリプトを読み込む

--- qa-custom-user-detail-layer.php
+++ qa-custom-user-detail-layer.php
@@ -4,6 +4,31 @@ require_once CUD_DIR.'/cud-theme-main.php';
 
 class qa_html_theme_layer extends qa_html_theme_base
 {
+    private $infscr = null;
+    
+    function qa_html_theme_layer($template, $content, $rooturl, $request)
+    {
+        qa_html_theme_base::qa_html_theme_base($template, $content, $rooturl, $request);
+        $this->infscr = new qa_infinite_scroll();
+    }
+    
+    function head_script()
+    {
+        qa_html_theme_base::head_script();
+        if (qa_opt('site_theme') === CUD_TARGET_THEME_NAME && $this->template === 'user') {
+            $this->output('<SCRIPT TYPE="text/javascript" SRC="'.$this->infscr->pluginjsurl.'jquery-ias.min.js"></SCRIPT>');
+            $this->output('<SCRIPT TYPE="text/javascript" SRC="'. QA_HTML_THEME_LAYER_URLTOROOT.'js/ias-user.js"></SCRIPT>');
+        }
+    }
+
+    function head_css()
+    {
+        qa_html_theme_base::head_css();
+        if (qa_opt('site_theme') === CUD_TARGET_THEME_NAME && $this->template === 'user') {
+            $this->output('<LINK REL="stylesheet" TYPE="text/css" HREF="'.$this->infscr->plugincssurl.'jquery.ias.css"/>');
+        }
+    }
+    
     public function body_content()
     {
         if (qa_opt('site_theme') === CUD_TARGET_THEME_NAME && $this->template === 'user') {
