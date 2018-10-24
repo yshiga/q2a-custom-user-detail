368c8cb560bb4e28d47c006ba02f4e49502c9b86
yaimapp <gmonday730-mbp@yahoo.co.jp>
2018-10-24 10:30:48 +0900

    不要な部分削除
    日誌のお気に入りをcontentに入れる

--- pages/user-detail.php
+++ pages/user-detail.php
@@ -344,81 +344,6 @@
 
                 $qa_content['form_profile']['fields']['level']['options'] = $leveloptions;
 
-
-            //    Category-specific levels
-
-                // if (qa_using_categories()) {
-                //     $catleveladd = strlen(qa_get('catleveladd')) > 0;
-                //
-                //     if ((!$catleveladd) && !count($userlevels)) {
-                //         $qa_content['form_profile']['fields']['level']['suffix'] = strtr(qa_lang_html('users/category_level_add'), array(
-                //             '^1' => '<a href="'.qa_path_html(qa_request(), array('state' => 'edit', 'catleveladd' => 1)).'">',
-                //             '^2' => '</a>',
-                //         ));
-                //     }
-                //     else
-                //         $qa_content['form_profile']['fields']['level']['suffix'] = qa_lang_html('users/level_in_general');
-                //
-                //     if ($catleveladd || count($userlevels))
-                //         $userlevels[] = array('entitytype' => QA_ENTITY_CATEGORY);
-                //
-                //     $index = 0;
-                    // foreach ($userlevels as $userlevel) {
-                    //     if ($userlevel['entitytype'] == QA_ENTITY_CATEGORY) {
-                    //         $index++;
-                    //         $id = 'ls_'.+$index;
-                    //
-                    //         $qa_content['form_profile']['fields']['uc_'.$index.'_level'] = array(
-                    //             'label' => qa_lang_html('users/category_level_label'),
-                    //             'type' => 'select',
-                    //             'tags' => 'name="uc_'.$index.'_level" id="'.qa_html($id).'" onchange="this.qa_prev=this.options[this.selectedIndex].value;"',
-                    //             'options' => $catleveloptions,
-                    //             'value' => isset($userlevel['level']) ? qa_html(qa_user_level_string($userlevel['level'])) : '',
-                    //             'suffix' => qa_lang_html('users/category_level_in'),
-                    //         );
-                    //
-                    //         $qa_content['form_profile']['fields']['uc_'.$index.'_cat'] = array();
-                    //
-                    //         if (isset($userlevel['entityid']))
-                    //             $fieldnavcategories = qa_db_select_with_pending(qa_db_category_nav_selectspec($userlevel['entityid'], true));
-                    //         else
-                    //             $fieldnavcategories = $navcategories;
-                    //
-                    //         qa_set_up_category_field($qa_content, $qa_content['form_profile']['fields']['uc_'.$index.'_cat'],
-                    //             'uc_'.$index.'_cat', $fieldnavcategories, @$userlevel['entityid'], true, true);
-                    //
-                    //         unset($qa_content['form_profile']['fields']['uc_'.$index.'_cat']['note']);
-                    //     }
-                    // }
-
-                //     $qa_content['script_lines'][] = array(
-                //         "function qa_update_category_levels()",
-                //         "{",
-                //         "\tglob=document.getElementById('level_select');",
-                //         "\tif (!glob)",
-                //         "\t\treturn;",
-                //         "\tvar opts=glob.options;",
-                //         "\tvar lev=parseInt(opts[glob.selectedIndex].value);",
-                //         "\tfor (var i=1; i<9999; i++) {",
-                //         "\t\tvar sel=document.getElementById('ls_'+i);",
-                //         "\t\tif (!sel)",
-                //         "\t\t\tbreak;",
-                //         "\t\tsel.qa_prev=sel.qa_prev || sel.options[sel.selectedIndex].value;",
-                //         "\t\tsel.options.length=1;", // just leaves "no upgrade" element
-                //         "\t\tfor (var j=0; j<opts.length; j++)",
-                //         "\t\t\tif (parseInt(opts[j].value)>lev)",
-                //         "\t\t\t\tsel.options[sel.options.length]=new Option(opts[j].text, opts[j].value, false, (opts[j].value==sel.qa_prev));",
-                //         "\t}",
-                //         "}",
-                //     );
-                //
-                //     $qa_content['script_onloads'][] = array(
-                //         "qa_update_category_levels();",
-                //     );
-                //
-                //     $qa_content['form_profile']['fields']['level']['tags'] .= ' id="level_select" onchange="qa_update_category_levels();"';
-                //
-                // }
             }
 
         }
@@ -439,29 +364,6 @@
 
         $showpermits = array();
         $permitoptions = qa_get_permit_options();
-
-        // foreach ($permitoptions as $permitoption) {
-        //     if ( // if not available to approved and email confirmed users with no points, but yes available to the user, it's something special
-        //         qa_permit_error($permitoption, $userid, QA_USER_LEVEL_APPROVED, QA_USER_FLAGS_EMAIL_CONFIRMED, 0) &&
-        //         !qa_permit_error($permitoption, $userid, $useraccount['level'], $useraccount['flags'], $userpoints['points'])
-        //     ) {
-        //         if ($permitoption == 'permit_retag_cat')
-        //             $showpermits[] = qa_lang(qa_using_categories() ? 'profile/permit_recat' : 'profile/permit_retag');
-        //         else
-        //             $showpermits[] = qa_lang('profile/'.$permitoption); // then show it as an extra priviliege
-        //     }
-        // }
-        //
-        // if (count($showpermits)) {
-        //     $qa_content['form_profile']['fields']['permits'] = array(
-        //         'type' => 'static',
-        //         'label' => qa_lang_html('profile/extra_privileges'),
-        //         'value' => qa_html(implode("\n", $showpermits), true),
-        //         'rows' => count($showpermits),
-        //         'id' => 'permits',
-        //     );
-        // }
-
 
     //    Show email address only if we're an administrator
 
@@ -631,13 +533,6 @@
         $questioncount = (int)@$userpoints['qposts'];
         $answercount = (int)@$userpoints['aposts'];
 
-        // $qa_content['q_list']['activities'] = include CUD_DIR.'/pages/user-activities.php';
-        // if (isset($activitiescount) && isset($pagesize)) {
-        //     $qa_content['page_links_activities'] = qa_html_page_links(qa_request(), $start, $pagesize, $activitiescount, qa_opt('pages_prev_next'), array('action' => 'activities'));
-        // } else {
-        //     $qa_content['page_links_activities'] = array();
-        // }
-
         $qa_content['q_list']['questions'] = include CUD_DIR.'/pages/user-questions.php';
         if (isset($questioncount) && isset($pagesize)) {
             $qa_content['page_links_questions'] = qa_html_page_links(qa_request(), $questions_start, $pagesize, $questioncount, qa_opt('pages_prev_next'), array('action' => 'questions'));
@@ -666,6 +561,13 @@
         } else {
             $qa_content['page_links_favorites'] = array();
         }
+
+        $qa_content['q_list']['blog-favorites'] = include CUD_DIR.'/pages/user-blog-favorites.php';
+        if (isset($blog_favorites_count) && isset($pagesize)) {
+            $qa_content['page_links_blog_favorites'] = qa_html_page_links(qa_request(), $blog_favorites_start, $pagesize, $blog_favorites_count, qa_opt('pages_prev_next'), array('action' => 'blog-favorites'));
+        } else {
+            $qa_content['page_links_blog_favorites'] = array();
+        }
     }
 
     $qa_content['counts'] = array();
@@ -673,6 +575,7 @@
     $qa_content['counts']['answers'] = $answercount;
     $qa_content['counts']['blogs'] = $blogcount;
     $qa_content['counts']['favorites'] = $favoritecount;
+    $qa_content['counts']['blog-favorites'] = $blog_favorites_count;
 
     $count = cud_utils::get_follows_count($useraccount['userid']);
     $qa_content['counts']['follows'] = $count['following'];
